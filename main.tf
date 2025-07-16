f6f550df0015aa09966e8aa51495f783ec1c428d
Samuel <ajalasamuel220@gmail.com>
2025-07-16 10:50:21 +0100

    Deploying a Configurable Web server

--- main.tf
+++ main.tf
@@ -15,7 +15,7 @@ resource "aws_instance" "test_server" {
 
                       mkdir -p /var/www
                       echo "Hello,World this is the first step we moving forward" > /var/www/index.html
-                      nohup busybox httpd -f -p 8080 -h /var/www &
+                      nohup busybox httpd -f -p ${var.server_port} -h /var/www &
                       EOF
   )
 
@@ -30,10 +30,20 @@ resource "aws_security_group" "internet" {
   name = "internet-connection"
 
   ingress {
-    from_port   = 8080
-    to_port     = 8080
+    from_port   = var.server_port
+    to_port     = var.server_port
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
 }
 
+variable "server_port" {
+  description = "The port the srver will use for HTTP requests."
+  type        = number
+  default     = 8080
+}
+
+output "public_ip" {
+  value       = aws_instance.test_server.public_ip
+  description = "The public IP address of the web server"
+}
